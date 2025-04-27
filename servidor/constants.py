8bb49893112df3bde7d8e9f3295dc352a4658971
Moisés Braz <126107557+MoisesBraz@users.noreply.github.com>
2025-04-27 23:44:07 +0100

    Constantes  para tipos de urgencias, tempo de atendimento, timeouts para um paciente deve demorar para atender, max de pacientes

---
+++ servidor/constants.py
@@ -0,0 +1,16 @@
+URGENCIA_PRIORIDADES = {
+    "vermelho": 0,
+    "amarelo": 1,
+    "verde": 2,
+}
+TEMPOS_ATENDIMENTO = {
+    "vermelho": 15,
+    "amarelo": 10,
+    "verde": 5,
+}
+TIMEOUTS = {
+    "vermelho": 240,
+    "amarelo": 120,
+    "verde": 60,
+}
+MAX_PACIENTES = 20
